3508badb2c0940b835d9df7008e03228e27c5aac
Lexi59 <trip9919@gmail.com>
2019-08-10 21:03:43 -0500

    added resizing

--- box.js
+++ box.js
@@ -1,19 +1,35 @@
 //16 columns
 //21 rows
-rectW = 20;
-rectH = 20;
+customW = 560;
+customH = 800;
 rows = 20;
-columns = 15;
+columns = 14;
+rectW = customH/rows;
+rectH = customW/columns;
+offset = rectW;
+startingPosition = [0,0];
 let img, answerRef, submissionRef, rawAnswerData;
 var boxes = new Array();
+var numberInputs = new Array();
 var answers = new Array();
 class box{
 	constructor(x,y,w,h){
 		this.x = x;
 		this.y = y;
-		this.w = w;
-		this.h = h;
 		this.checked = false;
+	}
+	show()
+	{
+		strokeWeight(2);
+		stroke(0);
+		if(this.checked){
+			fill("red");
+		}
+		else
+		{
+			noFill();
+		}
+		rect(this.x + startingPosition[0],this.y + startingPosition[1],rectW,rectH);
 	}
 }
 class entry{
@@ -51,62 +67,59 @@ function setup(){
      var database = firebase.database();
      answerRef = database.ref('answers');
      submissionRef = database.ref('submissions');
-     answerRef.on('value',updateAnswers);
+     answerRef.once('value').then(updateAnswers);
      //END FIREBASE
 	createCanvas(windowWidth,windowHeight);
 	colorMode(RGB);
 	var fileInput = createFileInput(selectImage);
-	fileInput.position(rectW * columns + 100, 50);
+	fileInput.position(rectW * columns + 105, 50);
 	background(255,255,255);
 	for(var c = 0; c < columns; c++)
 	{
 		var row = new Array();
 		for(var r = 0; r < rows; r++)
 		{
-			var b = new box(c*rectW,r*rectH,rectW,rectH);
+			var b = new box(c*rectW + offset+5,r*rectH,rectW,rectH);
 			row.push(b);
 		}
 		boxes.push(row);
+	}
+	for (var i = 0; i < rows; i++)
+	{
+		inp = createInput();
+		inp.position(0,i*rectH);
+		inp.size(offset,rectH);
+		numberInputs.push(inp);
 	}
 	var tabString = "Answer Tab"
 	if (findTab() == "Answer")
 	{
 		tabString = "Submission Tab"
 		saveButton = createButton("Save");
-		saveButton.position(rectW*columns+100,75);
+		saveButton.position(rectW*columns+105,75);
 		saveButton.mousePressed(saveAnswers);
 	}
 	else
 	{
 		scoreButton = createButton("Score");
-		scoreButton.position(rectW*columns + 100, 75);
+		scoreButton.position(rectW*columns + 105, 75);
 		scoreButton.mousePressed(calculateScore);
 	}
 	answerTabbutton = createButton(tabString);
-  	answerTabbutton.position(rectW*columns + 100, 25);
+  	answerTabbutton.position(rectW*columns + 105, 25);
   	answerTabbutton.mousePressed(switchTab);
 }
-function draw(){
-	strokeWeight(2);
-	stroke("black");
-	if (img) 
+function draw()
+{
+	clear();
+	checkKeys();
+	if(img){image(img, offset, 0, rectW*columns, rectH*rows);}
+	for (var r = 0; r < rows; r++)
 	{
-    	image(img, 0, 0, rectW*columns, rectH*rows);
-  	}
-	for(var r = 0; r < rows; r++)
-	{
+
 		for(var c = 0; c < columns; c++)
 		{
-			var b = boxes[c][r];
-			if(b.checked)
-			{
-				fill("red");
-			}
-			else
-			{
-				noFill();
-			}
-			rect(b.x,b.y,b.w,b.h);			
+			boxes[c][r].show();
 		}
 	}
 }
@@ -128,9 +141,9 @@ function selectImage(file)
     	img = createImg(file.data);
     	img.hide();
   	} 
-  	else 
+  	else
   	{
-    	img = null;
+  		img= null;
   	}
 }
 function mousePressed(){
@@ -150,7 +163,43 @@ function mousePressed(){
 		}
 	}
 }
-
+function checkKeys()
+{
+	if(keyIsDown(87))
+	{
+		startingPosition[1] -= 1;
+		if(startingPosition[1] < 0)
+			startingPosition[1] = 0
+	}
+	if(keyIsDown(83))
+	{
+		startingPosition[1]+=1;
+		if(startingPosition[1] > windowHeight)
+			startingPosition[1] = windowHeight;
+	}
+	if(keyIsDown(65))
+	{
+		startingPosition[0] -= 1;
+		if(startingPosition[0] < 0)
+			startingPosition[0] = 0;
+	}
+	if(keyIsDown(68))
+	{
+		startingPosition[0]+=1;
+		if(startingPosition[0] > windowWidth)
+			startingPosition[0] = windowWidth;
+	}
+	if(keyIsDown(88)){
+		customW += 2;
+		customH += 2;
+		sizeChange();
+	}
+	if(keyIsDown(90)){
+		customH -=2;
+		customW -=2;
+		sizeChange();
+	}
+}
 function findTab()
 {
 	if( window.location.href.indexOf('answerTab.html') >= 0)
@@ -169,68 +218,68 @@ function calculateScore()
 	{
 		var answer = answers[r];
 		//if plant number is wrong, skip the row
-		if (boxes[0][r].checked)
+		if (numberInputs[r].value()==answer.plantNum)
 		{
 			score += 8;
 			//Life Span section
-			if(boxes[1][r].checked && answer.perennial)
+			if(boxes[0][r].checked && answer.perennial)
 			{
 				score += 2;
 			}
-			else if (boxes[2][r].checked && answer.biennial)
+			else if (boxes[1][r].checked && answer.biennial)
 			{
 				score += 2;
 			}
-			else if (boxes[3][r].checked && answer.annual)
+			else if (boxes[2][r].checked && answer.annual)
 			{
 				score += 2;
 			}
 			//Growth season section
-			if(boxes[4][r].checked && answer.cool)
+			if(boxes[3][r].checked && answer.cool)
 			{
 				score += 2;
 			}
-			else if (boxes[5][r].checked && answer.warm)
+			else if (boxes[4][r].checked && answer.warm)
 			{
 				score += 2;
 			}
 			//origin
-			if(boxes[6][r].checked && !boxes[8][r].checked &&answer.native && !answer.invader)
+			if(boxes[5][r].checked && !boxes[7][r].checked &&answer.native && !answer.invader)
 			{
 				score += 2;
 			}
-			else if (boxes[6][r].checked && boxes[8][r].checked && answer.native && answer.invader)
+			else if (boxes[5][r].checked && boxes[7][r].checked && answer.native && answer.invader)
 			{
 				score += 2;
 			}
-			else if (boxes[7][r].checked && boxes[8][r].checked && answer.introduced && answer.invader)
+			else if (boxes[6][r].checked && boxes[7][r].checked && answer.introduced && answer.invader)
 			{
 				score += 2;
 			}
 			//prairie Grouse food
-			if (boxes[9][r].checked && answer.grouseFoodDe)
+			if (boxes[8][r].checked && answer.grouseFoodDe)
 			{
 				score += 2;
 			}
-			else if (boxes[10][r].checked && answer.grouseFoodUn)
+			else if (boxes[9][r].checked && answer.grouseFoodUn)
 			{
 				score += 2;
 			}
 			//prairie grouse cover
-			if (boxes[11][r].checked && answer.grouseCoverDe)
+			if (boxes[10][r].checked && answer.grouseCoverDe)
 			{
 				score += 2;
 			}
-			else if (boxes[12][r].checked && answer.grouseCoverUn)
+			else if (boxes[11][r].checked && answer.grouseCoverUn)
 			{
 				score += 2;
 			}
 			//cattle food
-			if (boxes[13][r].checked && answer.cattleFoodDe)
+			if (boxes[12][r].checked && answer.cattleFoodDe)
 			{
 				score += 2;
 			}
-			else if (boxes[14][r].checked && answer.cattleFoodUn)
+			else if (boxes[13][r].checked && answer.cattleFoodUn)
 			{
 				score += 2;
 			}
@@ -249,21 +298,21 @@ function saveAnswers()
 		}
 		var data = {
 			flagNum: r+1,
-			plantNum: row[0],
-			perennial: row[1],
-			biennial: row[2],
-			annual: row[3],
-			cool: row[4],
-			warm: row[5],
-			native: row[6],
-			introduced: row[7],
-			invader: row[8],
-			grouseFoodDe: row[9],
-			grouseFoodUn: row[10],
-			grouseCoverDe: row[11],
-			grouseCoverUn: row[12],
-			cattleFoodDe: row[13],
-			cattleFoodUn: row[14]
+			plantNum: numberInputs[r].value(),
+			perennial: row[0],
+			biennial: row[1],
+			annual: row[2],
+			cool: row[3],
+			warm: row[4],
+			native: row[5],
+			introduced: row[6],
+			invader: row[7],
+			grouseFoodDe: row[8],
+			grouseFoodUn: row[9],
+			grouseCoverDe: row[10],
+			grouseCoverUn: row[11],
+			cattleFoodDe: row[12],
+			cattleFoodUn: row[13]
 		}
 		answerRef.push(data)
 	}
@@ -277,4 +326,18 @@ function updateAnswers(data)
 		answers.push(new entry(keys[i]));
 	}
 	console.log(answers);
+}
+function sizeChange(){
+	boxes.
+	rectW = customH/rows;
+	rectH = customW/columns;
+	for(var c = 0; c < columns; c++)
+	{
+		for(var r = 0; r < rows; r++)
+		{
+			var b = boxes[c][r];
+			b.x = c * rectW + offset + 5;
+			b.y = r * rectH;
+		}
+	}
 }
